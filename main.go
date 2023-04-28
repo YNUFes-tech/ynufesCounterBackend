8fdfddc5f9f5dc95d994e9fa6c2cf998cc4572c5
Shion Ichikawa <shion1305@gmail.com>
2023-04-29 01:27:11 +0900

    ✨ デモコードの追加

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"google.golang.org/appengine"
+)
+
+func main() {
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/api/hello", handleHello)
+
+	appengine.Main()
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the main page!")
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, from the API!")
 }
