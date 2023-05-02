e1a9f0e0de4546ca0c94110238a270fe1b143257
Shion Ichikawa <shion1305@gmail.com>
2023-05-03 01:38:38 +0900

    🎨 (domain)Gate

---
+++ domain/gate.go
@@ -0,0 +1,12 @@
+package domain
+
+type Gate int
+
+const (
+	Main        Gate = 1
+	North       Gate = 2
+	West        Gate = 3
+	South       Gate = 4
+	SouthCommon Gate = 5
+	EastCommon  Gate = 6
+)
